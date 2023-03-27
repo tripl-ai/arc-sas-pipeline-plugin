b8199e932a793293bfb8af189a41c04ef679f78b
John Bruce <john.a.m.bruce@gmail.com>
2023-03-26 21:00:13 -0400

    Update to Arc 3.13.2 (#1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object Dependencies {
   // versions
-  lazy val sparkVersion = "3.0.1"
-  lazy val hadoopVersion = "3.2.0"
+  lazy val sparkVersion = "3.3.2"
+  lazy val hadoopVersion = "3.3.4"
 
   // testing
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % "test,it"
   val hadoopCommon =  "org.apache.hadoop" % "hadoop-common" % hadoopVersion % "it"
 
   // arc
-  val arc = "ai.tripl" %% "arc" % "3.7.0" % "provided"
+  val arc = "ai.tripl" %% "arc" % "3.13.2" % "provided"
 
   // spark
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
